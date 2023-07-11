b025fbc1f8f1d4e707e5d69e5443cdbc91fb711b
“suresh.akidev” <sureshr.tamizan@gmail.com>
2023-07-11 09:59:20 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -27,18 +27,6 @@ resource "aws_instance" "ec2_test-01" {
   subnet_id = "subnet-00d2838782fa3ecee"
   vpc_security_group_ids = ["sg-075d9dc319f0a8390"]
   tags = {
-    Name = "CICD"
+    Name = "CICD-autotrigger"
   }
-}
-
-
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket-157673692367"
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-    depends_on = [
-    aws_instance.ec2_test-01
-  ]
 }
