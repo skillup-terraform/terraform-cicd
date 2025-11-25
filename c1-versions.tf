4bce3dfa9ffb0ad17e8ce24027841ec710f1c65e
suresh-akidev <48638555+suresh-akidev@users.noreply.github.com>
2025-11-25 22:11:24 +0530

    Update c1-versions.tf

--- c1-versions.tf
+++ c1-versions.tf
@@ -6,12 +6,6 @@ terraform {
       version = "~> 3.0"
     }
   }
-    backend "s3" {
-    bucket = "terra-157673692367"
-    key    = "dev/terraform.tfstate"
-    region = "ap-south-1"
-    dynamodb_table = "terraform-state-lock"
-  }
 }
 
 # Provider Block
